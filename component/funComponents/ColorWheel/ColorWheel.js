0203ec4363946e5f36fe661e2f59edee1f816c2b
刘菁 <jing.liu@morelinks.cn>
2020-10-30 20:13:36 +0800

    111

--- component/funComponents/ColorWheel/ColorWheel.js
+++ component/funComponents/ColorWheel/ColorWheel.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { View, Button, Text, SafeAreaView, Dimensions, StyleSheet, FlatList, TouchableHighlight, Image } from 'react-native';
 import Wheel from '../../plugIn/ColorWheel'
+import ColorWheelNew from './ColorWheelNew'
 import colorsys from '../../../src/utils/colorsys'
 export default class ColorWheel extends React.Component {
     constructor(props) {
@@ -18,16 +19,25 @@ export default class ColorWheel extends React.Component {
         let { backgroundColor } = this.state
         return (
             <View style={{ backgroundColor: "#fff", height: '100%', justifyContent: "center", alignItems: "center", flex: 1 }}>
-                <View style={{ justifyContent: "center", alignItems: "center", height: 60}}>
+                <View style={{ justifyContent: "center", alignItems: "center", height: 60 }}>
                     <Text>当前的颜色是:&nbsp;&nbsp;{backgroundColor}</Text>
                 </View>
-                <View style={{ width: 200, height: 200, justifyContent: "center", alignItems: "center" }}>
+                <View style={{ width: 200, height: 400, justifyContent: "center", alignItems: "center" }}>
                     <Wheel
                         initialColor={backgroundColor}
                         onColorChange={this.changeColor}
                         style={{ width: Dimensions.get('window').width, backgroundColor: backgroundColor }}
                         thumbSize={30}
+
                     />
+                    <ColorWheelNew
+                        color={backgroundColor}
+                        // swatchesOnly={this.state.swatchesOnly}
+                        onColorChange={this.changeColor}
+                        onColorChangeComplete={this.changeColor}
+                        thumbSize={40}
+                        sliderSize={40}
+                        noSnap={true} />
                 </View>
             </View>
         );
